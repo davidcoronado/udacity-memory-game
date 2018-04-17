3930eddef6624845ac430d99bc4651c6bdd8afe8
david <davidjcoronado@gmail.com>
2018-04-16 20:21:03 -0700

    Add JS for Modal Pop up

--- js/app.js
+++ js/app.js
@@ -26,7 +26,7 @@
 let deck = document.querySelectorAll('.deck');
 let cardSelected;
 let openCards = [];
-let matchCount = [];
+let matchCount = 0;
 let cardClicked = [];
 let moves = document.querySelector('.moves');
 let star = document.querySelectorAll('.fa-star');
@@ -35,36 +35,44 @@ let secondStar = star[1];
 let thirdStar = star[2];
 let secondsTimer = 0;
 let minutesTimer = 0;
-let timer = document.querySelector('.timer');
-let seconds = document.querySelector('.seconds');
-let minutes = document.querySelector('.minutes');
+let timer = document.querySelectorAll('.timer');
+let seconds = document.querySelectorAll('.seconds');
+let minutes = document.querySelectorAll('.minutes');
 let firstTimeCardClick = false;
-let deckOfCards = [ 'fa fa-anchor','fa fa-bolt', 'fa-cube','fa fa-anchor',
+let deckOfCards = [ 'fa fa-anchor','fa fa-bolt', 'fa fa-cube','fa fa-anchor',
   'fa fa-leaf','fa fa-bicycle','fa fa-diamond','fa fa-bomb',
   'fa fa-leaf','fa fa-bomb','fa fa-bolt','fa fa-bicycle',
   'fa fa-paper-plane-o','fa fa-cube', 'fa fa-paper-plane-o', 'fa fa-diamond'
 ];
 let restart = document.querySelector('.restart');
 let cardValues = document.querySelectorAll('.deck .fa');
-
+let matchingCardz = document.querySelectorAll('.deck li'); //select all cards .className in the deck
+let totalTime = [];
 
 //List of EventListeners
 for(var i = 0; i < deck.length; i++){
     deck[i].addEventListener('click', function(event){
+      if (openCards.length > 1 ){
+        return;
+      }
       cardSelected = event.target;
+      if (openCards[0] === cardSelected){
+        console.log("card had already been selected")
+        return;
+      }
       cardClicked++;
       if (firstTimeCardClick === false){
           firstTimeCardClick = true;
+          matchCount = 0;
           setTimeout(timerStopWatch,1000);
+          console.log("timer turned on")
       };
       console.log('card Selected');
       cardFlip(cardSelected);
       cardsOpened(cardSelected);
       if(openCards.length > 1) {
-        cardsMatch();
-      }
-      else if(matchCount => 8 ){
         console.log("all cards match");
+        cardsMatch();
       }
       cardClickedTotal();
       starRating(cardClicked);
@@ -72,21 +80,34 @@ for(var i = 0; i < deck.length; i++){
 
 restart.addEventListener('click', function(){
   resetCards();
-  //TODO need to rest timer
-  //TODO need to reset moves
-  //TODO need to reset starRating
+  matchCount = 8;
+  for(var o = 0; o < matchingCardz.length; o++){
+  matchingCardz[o].className = "card"
+  }
+  timerReset();
+  movesReset();
+  starRatingReset();
+  firstTimeCardClick = false;
 });
-
-//TODO NEW EVENT LISTENER
-//possibly add event listener on Page load to reset cards
-
-
-//TODO Add Event Listener for module popup to play a new game
-
 
 
 
 //List of Functions
+function timerReset(){
+  timer[0].textContent = "Timer 0:00";
+};
+
+function movesReset(){
+  moves.textContent = "0";
+};
+
+function starRatingReset(){
+  firstStar.classList = "fa fa-star";
+  secondStar.classList = "fa fa-star";
+  thirdStar.classList = "fa fa-star";
+  cardClicked = 0;
+};
+
 function resetCards(){
 	shuffle(deckOfCards);
   for(var i = 0; i < cardValues.length; i++){
@@ -110,7 +131,6 @@ function cardsMatch(){
   let cardTwo = openCards[1].firstElementChild.className;
   console.log(cardOne);
   console.log(cardTwo);
-
   if(cardOne === cardTwo){
     setTimeout(matchingCards,1025);
     console.log("cards match");
@@ -151,13 +171,9 @@ function starRating(x){
       console.log("More than 20 moves")
       break;
   }
-  /*
-  depending on the number of moves give the user a score for example:
-   Less than 5 moves === 3 Stars,
-   Less than 8 moves and more than 5 moves === 2 Stars,
-   Less than 12 moves and more than 8 moves === 1 Star,
-   More than 13 moves === 0 Stars
-  */
+  star[3].classList = star[0].className;
+  star[4].classList = star[1].className;
+  star[5].classList = star[2].className;
 };
 
 function clearCards(){
@@ -182,17 +198,34 @@ function matchingCards(){
   openCards[1].classList += " match";
   //Resets the openCards array back to empty
   openCards = [];
+  if(matchCount === 8 ){
+    allCardsMatch();
+    console.log("all cards match");
+  }
 };
 
+
+//If All Cards Match
 function allCardsMatch(){
-/*
-if all li.card classes are "card open show" then all of the cards match
-*/
+  $('#myModal').modal('show')
+  resetCards();
+  for(var o = 0; o < matchingCardz.length; o++){
+  matchingCardz[o].className = "card"
+  }
+  timerReset();
+  movesReset();
+  starRatingReset();
+  firstTimeCardClick = false;
 };
+
 
 // Timer Function
 function timerStopWatch(){
   if (matchCount === 8){
+    totalTime = [minutesTimer,secondsTimer];
+    timer[1].textContent = "Timer " + totalTime[0] + ":" + totalTime[1];
+    secondsTimer = 0;
+    minutesTimer = 0;
     return;
   }
 secondsTimer = secondsTimer + 1;
@@ -200,8 +233,9 @@ setTimeout(timerStopWatch,1000);
 if (secondsTimer === 60) {
   secondsTimer = 0;
   minutesTimer = minutesTimer + 1};
-  timer.textContent = "Timer " + minutesTimer + ":" + secondsTimer;
+  timer[0].textContent = "Timer " + minutesTimer + ":" + secondsTimer;
 };
+
 
 function playNewGame(){
 /*
@@ -210,6 +244,7 @@ cardReset();
 make card disappear
 */
 };
+
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
